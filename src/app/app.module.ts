ac9caeadcc4a2d8a799d30f49068198c7c15830a
KrystsinaKastsiuk <kristina.v.kostyuk@gmail.com>
2017-11-20 04:20:15 +0300

    Task2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,19 @@ import { AppComponent } from './app.component';
 
 import { ProductComponent } from './components/product/product.component';
 import { ProductListComponent, ProductsService } from './components/product-list';
-import { CartService, CartComponent } from './cart';
+import { CartService, CartComponent } from './components/cart';
 import { Product, Category } from './components/product/';
+import { Item, CartItemComponent} from './components/cart-item/';
+import { Hello } from './components/hello/hello.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     ProductListComponent,
-    CartComponent
+    CartComponent,
+    CartItemComponent,
+    Hello
   ],
   imports: [
     BrowserModule,
--- src/app/components/product-list/product-list.service.ts
+++ src/app/components/product-list/product-list.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { Product, Category } from '../product/product.model';
 
-@Injectable()
+@Injectable() // decorator tells Angular that this service might itself have injected dependencies
 export class ProductsService {
 
   constructor() { }
@@ -10,7 +10,8 @@ export class ProductsService {
   getProducts(): Array<Product> {
     return [
       new Product('Bread', 'This is my first bread', 3, Category.A, true, ['flour', 'water'], ['bun', 'sandwich'], false),
-      new Product('Milk', 'Very tasty milk', 6, Category.B, true, ['cow'], ['kefir', 'ryazhenka'], false)
+      new Product('Milk', 'Very tasty milk', 6, Category.B, true, ['cow'], ['kefir', 'ryazhenka'], false),
+      new Product('Candies', 'Sweet candies', 6, Category.A, false, ['chocolate', 'milk'], ['marshmallows', 'wafers'], false)
     ];
   }
 
